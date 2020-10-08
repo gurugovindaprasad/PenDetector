bac2f8a683f1e8efb0b237c7c70a92dcdf65b5ff
gurugovindaprasad <gurugovindaprasad@gmail.com>
2020-10-08 18:42:19 +0530

    First Version of  the app to detect different colors of pens.

--- PenDetector/ViewController.swift
+++ PenDetector/ViewController.swift
@@ -6,14 +6,173 @@
 //
 
 import UIKit
+import TensorFlowLite
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var previewView: PreviewView!
+    @IBOutlet weak var overlayView: OverlayView!
+    
+    // MARK: Constants
+    private let displayFont = UIFont.systemFont(ofSize: 14.0, weight: .medium)
+    private let edgeOffset: CGFloat = 2.0
+    private let labelOffset: CGFloat = 10.0
+    private let animationDuration = 0.5
+    private let collapseTransitionThreshold: CGFloat = -30.0
+    private let expandThransitionThreshold: CGFloat = 30.0
+    private let delayBetweenInferencesMs: Double = 200
+    
+    // Holds the results at any time
+    private var result: Result?
+    private var previousInferenceTimeMs: TimeInterval = Date.distantPast.timeIntervalSince1970 * 1000
+    
+    private var modelDataHandler: ModelDataHandler? =
+      ModelDataHandler(modelFileInfo: MobileNetSSD.modelInfo, labelsFileInfo: MobileNetSSD.labelsInfo)
+    private lazy var cameraFeedManager = CameraFeedManager(previewView: previewView)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard modelDataHandler != nil else {
+          fatalError("Failed to load model")
+        }
+        
+        cameraFeedManager.delegate = self
+        overlayView.clearsContextBeforeDrawing = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+        
+      // Starting the Camera Session
+      cameraFeedManager.checkCameraConfigurationAndStartSession()
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+      super.viewWillDisappear(animated)
+      // Stopping the camera
+      cameraFeedManager.stopSession()
+    }
+    
+    
 
+}
+
+
+extension ViewController: CameraFeedManagerDelegate {
+    func didOutput(pixelBuffer: CVPixelBuffer) {
+        runModel(onPixelBuffer: pixelBuffer)
+    }
+    
+    func presentCameraPermissionsDeniedAlert() {
+        // add a alert if camera permission is denied
+    }
+    
+    func presentVideoConfigurationErrorAlert() {
+        // alert if camera configuration is failed.
+    }
+    
+    func sessionRunTimeErrorOccured() {
+        
+    }
+    
+    func sessionWasInterrupted(canResumeManually resumeManually: Bool) {
+        
+    }
+    
+    func sessionInterruptionEnded() {
+        
+    }
+    
+    
+    /** This method runs the live camera pixelBuffer through tensorFlow to get the result.
+     */
+    @objc  func runModel(onPixelBuffer pixelBuffer: CVPixelBuffer) {
+
+      // Run the live camera pixelBuffer through tensorFlow to get the result
+
+      let currentTimeMs = Date().timeIntervalSince1970 * 1000
+
+      guard  (currentTimeMs - previousInferenceTimeMs) >= 200 else {
+        return
+      }
+
+      previousInferenceTimeMs = currentTimeMs
+      result = self.modelDataHandler?.runModel(onFrame: pixelBuffer)
+
+      guard let displayResult = result else {
+        return
+      }
+
+      let width = CVPixelBufferGetWidth(pixelBuffer)
+      let height = CVPixelBufferGetHeight(pixelBuffer)
+
+      DispatchQueue.main.async {
+
+        // Draws the bounding boxes and displays class names and confidence scores.
+        self.drawAfterPerformingCalculations(onInferences: displayResult.inferences, withImageSize: CGSize(width: CGFloat(width), height: CGFloat(height)))
+      }
+    }
+
+    /**
+     This method takes the results, translates the bounding box rects to the current view, draws the bounding boxes, classNames and confidence scores of inferences.
+     */
+    func drawAfterPerformingCalculations(onInferences inferences: [Inference], withImageSize imageSize:CGSize) {
+
+      self.overlayView.objectOverlays = []
+      self.overlayView.setNeedsDisplay()
+
+      guard !inferences.isEmpty else {
+        return
+      }
+
+      var objectOverlays: [ObjectOverlay] = []
+
+      for inference in inferences {
+
+        // Translates bounding box rect to current view.
+        var convertedRect = inference.rect.applying(CGAffineTransform(scaleX: self.overlayView.bounds.size.width / imageSize.width, y: self.overlayView.bounds.size.height / imageSize.height))
+
+        if convertedRect.origin.x < 0 {
+          convertedRect.origin.x = self.edgeOffset
+        }
+
+        if convertedRect.origin.y < 0 {
+          convertedRect.origin.y = self.edgeOffset
+        }
+
+        if convertedRect.maxY > self.overlayView.bounds.maxY {
+          convertedRect.size.height = self.overlayView.bounds.maxY - convertedRect.origin.y - self.edgeOffset
+        }
+
+        if convertedRect.maxX > self.overlayView.bounds.maxX {
+          convertedRect.size.width = self.overlayView.bounds.maxX - convertedRect.origin.x - self.edgeOffset
+        }
+
+        let confidenceValue = Int(inference.confidence * 100.0)
+        let string = "\(inference.className)  (\(confidenceValue)%)"
+
+        let size = string.size(usingFont: self.displayFont)
+
+        let objectOverlay = ObjectOverlay(name: string, borderRect: convertedRect, nameStringSize: size, color: inference.displayColor, font: self.displayFont)
+
+        objectOverlays.append(objectOverlay)
+      }
+
+      // Hands off drawing to the OverlayView
+      self.draw(objectOverlays: objectOverlays)
+
+    }
+
+    /** Calls methods to update overlay view with detected bounding boxes and class names.
+     */
+    func draw(objectOverlays: [ObjectOverlay]) {
+
+      self.overlayView.objectOverlays = objectOverlays
+      self.overlayView.setNeedsDisplay()
+    }
+    
+    
 }
 
